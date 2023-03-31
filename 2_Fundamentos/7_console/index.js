61fe007e8a247f2ca8ba4648b97509c4957f65b1
João Henrique Almeida Xavier <63925427+jhenriqueax@users.noreply.github.com>
2023-03-31 14:55:15 -0300

    final

---
+++ 2_Fundamentos/7_console/index.js
@@ -0,0 +1,23 @@
+const x = 10
+const y = "João Henrique"
+const z = [1, 2]
+
+console.log(x,y,z)
+
+//console com contagem
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+console.count(`o valor de x é: ${x}, contagem`)
+
+//variavel com String
+
+console.log("\n o nome dele é: %s", y)
+
+//limpar console (terminal)
+setTimeout(()=>{
+
+    console.clear()
+
+}, 2000)
---
+++ 2_Fundamentos/8_chalk/index.js
@@ -0,0 +1,5 @@
+const chalk = require("./node_modules/chalk")
+
+const nota = 9
+
+console.log(chalk.green("parabéns você passou"))
